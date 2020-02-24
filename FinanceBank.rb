81285b0432486fb91513305e0e19d210a41d5f6f
Manish21-v <vitkarmanish21@gmail.com>
2020-02-24 15:42:02 +0530

    Deleted all the repeated code

--- FinanceBank.rb
+++ FinanceBank.rb
@@ -11,10 +11,10 @@ class Account
             @account_id=id;
             @customer_name=name;
             @balance=balance
-            @customer_email="#{name}+@gmail.com";
+            @customer_email="#{name}@gmail.com";
         end
     
-        def fixeddeposite()
+        def fixeddeposit()
             puts "enter how much money you want to invest in FD" 
             fd = gets.chomp.to_i
             totalamount = @balance.to_i - fd
@@ -22,12 +22,14 @@ class Account
             puts "now your remaining amount is :"+totalamount.to_s+" and your FD is :"+fd.to_s
 
             @account_Type="Saving";
-        end
+          end
+
+
         def show_interestRate()
             amount=@balance*Interest_rate;
             puts " Your interest on the Balance : #{@balance +amount}";
         end
-
+        
         def show_account_details
             puts "----------Account Details----------"
             puts "ID: #{@account_id}"
@@ -36,50 +38,20 @@ class Account
             puts "Account Type: #{@account_Type}"
             puts "Current Balance: #{@balance}"
         end
-  Interest_rate=0.06; 
 
-  def initialize(id,name,balance)
-    @account_id=id;
-    @customer_name=name;
-    @balance=balance
-    @customer_email="#{name}+@gmail.com";
-    @account_Type="Saving";
-  end
-    
-  def show_interestRate()
-      amount=@balance*Interest_rate;
-      puts " Your interest on the Balance : #{@balance +amount}";
-  end
+  
+        def deposit(acc_no,amount,type)
+          if acc_no==@account_id and type.eql?(@account_Type)
+          @balance=@balance+amount
+          puts "Amount is Deposited in Account"
+          print "Now Balance is :-> ",@balance,"\n"
+        end
+      end
 
-  def deposit(acc_no,amount,type)
-    if acc_no==@account_id and type.eql?(@account_Type)
-      @balance=@balance+amount
-      puts "Amount is Deposited in Account"
-      print "Now Balance is :-> ",@balance,"\n"
+
     end
-  end
-  def show_interestRate()
-      amount=@balance*Interest_rate;
-      puts " Your interest on the Balance : #{@balance +amount}";
-  end
-
-  def show_account_details
-      puts "----------Account Details----------"
-      puts "ID: #{@account_id}"
-      puts "Customer Name: #{@customer_name}"
-      puts "Customer Email: #{@customer_email}"
-      puts "Account Type: #{@account_Type}"
-      puts "Current Balance: #{@balance}"
-  end
-end
 Acc=Account.new(1000,"Ramesh",5000);
 Acc.show_interestRate();
-
-
-Acc.fixeddeposite()
-
+Acc.fixeddeposit()
 Acc.show_account_details()
 
-
-
-Acc.show_account_details()
