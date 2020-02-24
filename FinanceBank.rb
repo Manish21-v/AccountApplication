8db2679b77d0fa0e38345046c0b16f76cdf5b11f
Subhajit <subhajit28061997@gmail.com>
2020-02-24 15:11:52 +0530

    checkbal

--- FinanceBank.rb
+++ FinanceBank.rb
@@ -1,18 +1,24 @@
 $interest_rate;
 class Account
-    @Account_id;
-    @Customer_name;
-    @Balance;
-    @Customer_email;
-    @Account_Type="Saving";
+    @account_id;
+    @customer_name;
+    @balance;
+    @customer_email;
+    @account_Type="Saving";
     
         def initialize(id,name,balance)
-            @Account_id=id;
-            @Customer_name=name;
-            @Balance=balance
-            @Customer_email="#{name}+@gmail.com";
+            @account_id=id;
+            @customer_name=name;
+            @balance=balance
+            @customer_email="#{name}+@gmail.com";
         end
-    
-
-
+        def checkbalance 
+            if @balance>1000
+                puts "balance is = #{@balance}"
+            else
+                puts "Not enough amount less than 1000 i.e. = #{@balance}"
+            end
+        end
 end
+acc=Account.new(11,"Josh",12000)
+acc.checkbalance
