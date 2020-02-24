241de1503d558e0a37d7d200cff12fd5b8074691
Manish21-v <vitkarmanish21@gmail.com>
2020-02-24 14:52:15 +0530

    Account class with show_interest_function

--- FinanceBank.rb
+++ FinanceBank.rb
@@ -1,18 +1,25 @@
-$interest_rate;
+
 class Account
-    @Account_id;
-    @Customer_name;
-    @Balance;
-    @Customer_email;
-    @Account_Type="Saving";
-    
+    Interest_rate=0.06; 
         def initialize(id,name,balance)
-            @Account_id=id;
-            @Customer_name=name;
-            @Balance=balance
-            @Customer_email="#{name}+@gmail.com";
+            @account_id=id;
+            @customer_name=name;
+            @balance=balance
+            @customer_email="#{name}+@gmail.com";
+            @account_Type="Saving";
         end
     
+        def show_interestRate()
+            amount=@balance*Interest_rate;
+            puts " Your interest on the Balance : #{@balance +amount}";
+        end
 
 
 end
+
+Acc=Account.new(1000,"Ramesh",5000);
+Acc.show_interestRate();
+
+
+
+
