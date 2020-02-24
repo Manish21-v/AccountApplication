b6457ed8193dddd3806f79184dff82c1630009f9
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-02-24 15:08:26 +0530

    FD added

--- FinanceBank.rb
+++ FinanceBank.rb
@@ -1,18 +1,30 @@
 $interest_rate;
 class Account
-    @Account_id;
-    @Customer_name;
-    @Balance;
-    @Customer_email;
-    @Account_Type="Saving";
+    @account_id;
+    @customer_name;
+    @balance;
+    @customer_email;
+    @account_Type="Saving";
     
         def initialize(id,name,balance)
-            @Account_id=id;
-            @Customer_name=name;
-            @Balance=balance
-            @Customer_email="#{name}+@gmail.com";
+            @account_id=id;
+            @customer_name=name;
+            @balance=balance
+            @customer_email="#{name}+@gmail.com";
         end
     
+        def fixeddeposite()
+            puts "enter how much money you want to invest in FD" 
+            fd = gets.chomp.to_i
+            totalamount = @balance.to_i - fd
+            puts totalamount.to_i
+            puts "now your remaining amount is :"+totalamount.to_s+" and your FD is :"+fd.to_s
+
+        end
 
 
 end
+
+acc = Account.new(10,"onkar",10000)
+
+acc.fixeddeposite()
