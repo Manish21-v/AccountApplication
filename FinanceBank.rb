07a29108742428552bf2ff9d450f4ff70dc7e712
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-02-24 15:11:51 +0530

    FD added to branch

--- FinanceBank.rb
+++ FinanceBank.rb
@@ -1,4 +1,4 @@
-$interest_rate;
+
 class Account
     @account_id;
     @customer_name;
@@ -6,6 +6,7 @@ class Account
     @customer_email;
     @account_Type="Saving";
     
+    Interest_rate=0.06; 
         def initialize(id,name,balance)
             @account_id=id;
             @customer_name=name;
@@ -20,6 +21,12 @@ class Account
             puts totalamount.to_i
             puts "now your remaining amount is :"+totalamount.to_s+" and your FD is :"+fd.to_s
 
+            @account_Type="Saving";
+        end
+    
+        def show_interestRate()
+            amount=@balance*Interest_rate;
+            puts " Your interest on the Balance : #{@balance +amount}";
         end
 
 
@@ -28,3 +35,9 @@ end
 acc = Account.new(10,"onkar",10000)
 
 acc.fixeddeposite()
+Acc=Account.new(1000,"Ramesh",5000);
+Acc.show_interestRate();
+
+
+
+
